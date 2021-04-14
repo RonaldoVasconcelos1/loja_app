147b8b0249d9645971c5a42f599d0999ad423142
Eduardo Canda <eduardocandac@gmail.com>
2021-04-14 01:16:50 -0300

    Method OnModelCreating

--- Loja.Repository/Data/ClientContext.cs
+++ Loja.Repository/Data/ClientContext.cs
@@ -12,7 +12,6 @@ namespace Loja.Repository.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Client>().HasIndex(c => c.Id);
-
             modelBuilder.Entity<Client>().ToTable("clients");
             modelBuilder.Entity<Client>().Property(c => c.Id).HasColumnName("client_id");
             modelBuilder.Entity<Client>().Property(c => c.Name).HasColumnName("client_name").HasMaxLength(60);
@@ -20,5 +19,6 @@ namespace Loja.Repository.Data
             modelBuilder.Entity<Client>().HasIndex(c => c.Cpf).IsUnique();
             modelBuilder.Entity<Client>().Property(c => c.BirthDate).HasColumnName("client_birth_date").HasColumnType("date");
         }
+
     }
 }
